f40d17821ddc45d589565b3d56725a62c85cce60
basdzxqwe <ock6166@naver.com>
2023-03-02 21:45:04 +0900

    데이터 리스트 조회

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 import express from "express";
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "korck",
+  password: "kor123414",
+  database: "wise_saying",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+})
 
 const app = express();
 const port = 3000;
 
 const wiseSayings = [
   {
-    content : "나는 의적이다.",
-    author : "홍길동",
+    content: "나는 의적이다.",
+    author: "홍길동",
   },
   {
-    content : "나는 도적이다.",
-    author : "임꺽정",
+    content: "나는 도적이다.",
+    author: "임꺽정",
   },
 ];
 
@@ -18,13 +29,27 @@ app.get('/', (req, res) => {
   res.send('Hello World!!!!!');
 });
 
-app.get('/wise-sayings', (req, res) => {
-    res.json(wiseSayings);
-  });
+app.get('/wise-sayings', async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM wise_saying ORDER BY id DESC")
 
-  app.get('/setting', (req, res) => {
-    res.send('Setting!');
-  });
+  res.json(rows);
+});
+
+app.get('/wise-sayings/:id', async (req, res) => {
+  const {id} = req.params;
+  const [rows] = await pool.query("SELECT * FROM wise_saying WHERE id = ?" , [
+    id
+  ]);
+
+  if ( rows.length == 0 ) {
+    res.status(404).send("not found");
+    return;
+  }
+
+  res.json(rows[0]);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
