bb69d4aaf776ba31ed4e18787d2c4b19ff64e68f
basdzxqwe <ock6166@naver.com>
2023-03-09 16:41:04 +0900

    express 적용

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-})
+});
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 const wiseSayings = [
@@ -25,23 +26,51 @@ const wiseSayings = [
   },
 ];
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!!!');
-});
-
-app.get('/wise-sayings', async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM wise_saying ORDER BY id DESC")
+app.get("/wise-sayings", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM wise_saying ORDER BY id DESC");
 
   res.json(rows);
 });
 
-app.get('/wise-sayings/:id', async (req, res) => {
-  const {id} = req.params;
-  const [rows] = await pool.query("SELECT * FROM wise_saying WHERE id = ?" , [
-    id
+app.post("/wise-sayings", async (req, res) => {
+  const { author, content } = req.body;
+
+  if (!author) {
+    res.status(400).json({
+      msg: "author required",
+    });
+    return;
+  }
+
+  if (!content) {
+    res.status(400).json({
+      msg: "content required",
+    });
+    return;
+  }
+
+  const [rs] = await pool.query(
+    `
+    INSERT INTO wise_saying
+    SET regDate = NOW(),
+    content = ?,
+    author = ?
+    `,
+    [content, author]
+  );
+
+  res.status(201).json({
+    id: rs.insertId,
+  });
+});
+
+app.get("/wise-sayings/:id", async (req, res) => {
+  const { id } = req.params;
+  const [rows] = await pool.query("SELECT * FROM wise_saying WhERE id = ?", [
+    id,
   ]);
 
-  if ( rows.length == 0 ) {
+  if (rows.length == 0) {
     res.status(404).send("not found");
     return;
   }
@@ -49,7 +78,75 @@ app.get('/wise-sayings/:id', async (req, res) => {
   res.json(rows[0]);
 });
 
+app.delete("/wise-sayings/:id", async (req, res) => {
+  const { id } = req.params;
+  const [rows] = await pool.query("SELECT * FROM wise_saying WhERE id = ?", [
+    id,
+  ]);
 
+  if (rows.length == 0) {
+    res.status(404).send("not found");
+    return;
+  }
+
+  const [rs] = await pool.query(
+    `
+    DELETE FROM wise_saying
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  res.status(200).json({
+    id,
+  });
+
+  res.json(rows[0]);
+});
+
+app.patch("/wise-sayings/:id", async (req, res) => {
+  const { id } = req.params;
+  const [rows] = await pool.query("SELECT * FROM wise_saying WhERE id = ?", [
+    id,
+  ]);
+  const { author, content } = req.body;
+
+  if (rows.length == 0) {
+    res.status(404).send("not found");
+    return;
+  }
+
+  if (!author) {
+    res.status(400).json({
+      msg: "author required",
+    });
+    return;
+  }
+
+
+  if (!content) {
+    res.status(400).json({
+      msg: "content required",
+    });
+    return;
+  }
+
+  const [rs] = await pool.query(
+    `
+    UPDATE wise_saying
+    SET content = ?,
+    author = ?
+    WHERE id = ?
+    `,
+    [content, author, id]
+  );
+
+  res.status(200).json({
+    id,
+    author,
+    content,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
